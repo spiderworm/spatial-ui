3376b560b09590bcd35c0344b9c0e98187a6865c
David Millet <david.millet@disney.com>
2014-02-16 08:31:50 -0800

    adding sandbox to test various features, and setting up a better way to share require.js configs

--- js/main.js
+++ js/main.js
@@ -4,53 +4,72 @@ if (window.require !== window.requirejs) {
 	window.require.nodeRequire = global.require;
 }
 
-var baseUrl = (function() {
-	var loc = document.location.href;
-	loc = loc.match(/^([^\?#]*\/).*$/)[1];
-	return loc;
+(function() {
+
+	var x = new XMLHttpRequest();
+	x.open('GET','js/require-config.json',false);
+	x.send();
+	var config = JSON.parse(x.responseText);
+	require.config(config);
+
+	var app;
+
+	require(
+		[
+			'./App'
+		],
+		function(App) {
+			app = new App();
+			app.start();
+		}
+	);
+
+	function API() {
+		var api = this;
+		this.__interval = setInterval(function() {
+			api.__checkReady();
+		}, 100);
+	}
+	API.prototype.getModel = function() {
+		if(app) {
+			return app.getModel();
+		}
+	}
+	API.prototype.setStoryConnectionInfo = function(url,connectionType,dataFormat,connect) {
+		if(this.isReady()) {
+			this.getModel().story.$update({
+				url: url,
+				connectionType: connectionType || 'websocket',
+				dataFormat: dataFormat || 'osc',
+				connect: connect ? true : false
+			});
+		}
+	}
+	API.prototype.connect = function() {
+		if(this.isReady()) {
+			this.getModel().story.$update({connect: true});
+		}
+	}
+	API.prototype.onReady = function(callback) {
+		this.__callbacks.push(callback);
+		this.__checkReady();
+	}
+	API.prototype.isReady = function() {
+		return app && app.isStarted();
+	}
+	API.prototype.__checkReady = function() {
+		if(this.isReady()) {
+			if(this.__interval) {
+				clearInterval(this.__interval);
+				delete this.__interval;
+			}
+			while(this.__callbacks) {
+				var callback = this.__callbacks.shift();
+				callback();
+			}
+		}
+	}
+
+	window.api = new API();
+
 })();
-
-require.config({
-	baseUrl: baseUrl + "js/",
-	paths: {
-		"react": "external/react",
-		"jsx": "external/require-jsx",
-		"JSXTransformer": "external/JSXTransformer",
-		'THREE': 'external/three',
-		'THREE.GeometryExporter': 'external/three.geometryExporter'
-	},
-	shim: {
-		'THREE': {
-			exports: 'THREE'
-		},
-		'THREE.GeometryExporter': {
-			exports: 'THREE.GeometryExporter',
-			deps: ['THREE']
-		},
-		'external/threex.keyboardstate': {
-			exports: 'THREEx.KeyboardState'
-		},
-		'external/OrbitControls': {
-			exports: 'THREE.OrbitControls',
-			deps: ['THREE']
-		}
-	}
-});
-
-require(
-	[
-		'./registry',
-		'./util/urlUtil'
-	],
-	function(
-		registry,
-		urlUtil
-	) {
-		registry.set('mock',true);
-
-		var vals = urlUtil.getQueryValues();
-		if(vals.mock !== undefined) {
-			registry.set('mock',vals.mock);
-		}
-	}
-);
